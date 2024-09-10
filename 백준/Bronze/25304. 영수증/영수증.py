3c749f8411378d9656ffc22546deaba1e67a4b54
simm <mjmj752002@naver.com>
2024-09-10 20:09:07 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+X = int(input())
+n = int(input())
+
+price = 0
+for i in range(n):
+    a, b = map(int,input().split())
+    price += a*b
+    
+if price == X:
+    print("Yes")
+else:
+    print("No")
